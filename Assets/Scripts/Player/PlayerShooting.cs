a0bf6907c60314821cdc11f07630c4058e1bf332
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-06-28 19:25:05 +0100

    feat: Melee Attack
    - Add: Melee Attack for when player runs out of blood in the blood tank

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -9,27 +9,50 @@ public class PlayerShooting : MonoBehaviour {
     [SerializeField] private Player player;
 
     [Header("Ammunition")]
-    [SerializeField] private int shotCost;
-    [SerializeField] private int shotScore;
+    [SerializeField] private int ammoCost;
+
+    [Header("Melee Attack")]
+    [SerializeField] private float meleeRange = 1f;
+    [SerializeField] private int meleeDamage = 10;
 
     void Update() {
         if (gameInput != null && gameInput.IsShooting()) {
+            ShootOrMelee();
+        }
+    }
+
+    void ShootOrMelee() {
+        if (player.UseBloodForShooting(ammoCost)) {
             Shoot();
+        } else {
+            MeleeAttack();
         }
     }
 
     void Shoot() {
-        if (player != null && player.UseBloodForShooting(shotCost)) {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 direction = (mousePosition - shootPosition.position).normalized;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - shootPosition.position).normalized;
 
-            GameObject projectile = Instantiate(playerProjectilePrefab, shootPosition.position, Quaternion.identity);
-            var playerProjectile = projectile.GetComponent<PlayerProjectile>();
-            playerProjectile.SetDirection(direction);
+        GameObject projectile = Instantiate(playerProjectilePrefab, shootPosition.position, Quaternion.identity);
+        var playerProjectile = projectile.GetComponent<PlayerProjectile>();
+        playerProjectile.SetDirection(direction);
+    }
 
-            player.AddScore(shotScore);
-        } else {
-            Debug.Log("Not enough blood in the tank to shoot!");
+    void MeleeAttack() {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, meleeRange);
+
+        foreach (Collider2D enemy in hitEnemies) {
+            if (enemy.CompareTag("Enemy")) {
+                Enemy enemyScript = enemy.GetComponent<Enemy>();
+                if (enemyScript != null) {
+                    enemyScript.TakeDamage(meleeDamage);
+                }
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, meleeRange);
     }
 }
