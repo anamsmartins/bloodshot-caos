4afbacf093f6ef05dea514d4b0b292cee2763ebc
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-06-28 20:27:13 +0100

    chore: Projectiles Refactor

--- Assets/Scripts/Projectile/PlayerProjectile.cs
+++ Assets/Scripts/Projectile/PlayerProjectile.cs
@@ -3,39 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour {
+    [Header("Projectile")]
     [SerializeField] private int speed;
     [SerializeField] private int damage;
-    private Vector2 direction;
-    private Animator myAnimator = null;
 
-    private void Awake()
-    {
-        myAnimator = GetComponent<Animator>();
+    private Vector2 direction;
+    private Animator animator;
+
+    private void Awake() {
+        InitializeAnimator();
+    }
+
+    private void InitializeAnimator() {
+        animator = GetComponent<Animator>();
     }
 
     private void Update() {
-        transform.position += (Vector3)direction * speed * Time.deltaTime;
+        MoveProjectile();
+    }
 
-        // add destroy the projectile after it hits the map wall
+    private void MoveProjectile() {
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        HandleCollision(other);
+    }
+
+    private void HandleCollision(Collider2D other) {
         if (other.CompareTag("Enemy")) {
-            Enemy enemy = other.GetComponent<Enemy>();
-            if (enemy != null) {
-                // Deal damage to the enemy
-                enemy.TakeDamage(damage);
-            }
-            DestroyProjectile();
+            DamageEnemy(other);
         } else if (other.CompareTag("MapBoundary")) {
             DestroyProjectile();
         }
     }
 
-    // Method to set the direction
-    public void SetDirection(Vector2 direction)
-    {
-        this.direction = direction.normalized;
+    private void DamageEnemy(Collider2D enemyCollider) {
+        Enemy enemy = enemyCollider.GetComponent<Enemy>();
+        if (enemy != null) {
+            enemy.TakeDamage(damage);
+        }
+        DestroyProjectile();
+    }
+
+    public void SetDirection(Vector2 newDirection) {
+        direction = newDirection.normalized;
     }
 
     private void DestroyProjectile() {
